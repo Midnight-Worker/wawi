6f2a34ac60809adc3eb0bb06a45e5890d35bb36b
Maik Tappe <maik.tappe82@googlemail.com>
2026-01-08 19:31:18 +0100

    new

---
+++ main.py
@@ -0,0 +1,16 @@
+import webview
+
+class Api:
+        def do_something(self):
+                print("did something")
+
+api=Api()
+
+webview.create_window(
+        "Wareneingang B7",
+        url="public/index.html",
+        fullscreen=True,
+        js_api=api
+)
+
+webview.start()
